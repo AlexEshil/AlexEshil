022cd29458b5210f57b6cc02a29faf5b9e1f3411
Alexander <alexruspro93@gmail.com>
2025-07-12 14:30:15 +0300

    Add files via upload

---
+++ TestBio/BiogenomAPI/BiogenomAPI/Program.cs
@@ -0,0 +1,31 @@
+﻿using BiogenomAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BiogenomAPI", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Swagger работает всегда (если хочешь ограничить только дев, оставь как было)
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BiogenomAPI v1");
+});
+
+app.UseHttpsRedirection();  // по желанию
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
